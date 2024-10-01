5ee9bb7f250bd74ae7553424c0d51dc1235823ab
rsaniceto14 <raphaelaniceto14@gmail.com>
2024-10-01 12:18:38 -0300

    [chore] adding libs to start login page

---
+++ app/_components/header.tsx
@@ -0,0 +1,29 @@
+import { MenuIcon } from "lucide-react";
+import { Sheet, SheetTrigger } from "./ui/sheet";
+import { Card, CardContent } from "./ui/card";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+const Header = () => {
+    return (
+
+        <Card>
+            <CardContent className="flex flex-row items-center justify-between p-5">
+                <Image alt="BrainAg" src="/3980036.png" height={18} width={60} />
+
+                <h1>Brain Agriculture</h1>
+
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button size="icon" variant="outline">
+                            <MenuIcon />
+                        </Button>
+                    </SheetTrigger>
+                </Sheet>
+            </CardContent>
+        </Card>
+
+    );
+}
+
+export default Header;
---
+++ app/_components/insert_producer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InsertProducerPage = () => {
+    return ( 
+
+        <>
+       
+        </>
+
+     );
+}
+ 
+export default InsertProducerPage;
